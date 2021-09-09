c46e35308f7c7ed65736a9c2ed0567963e2245ec
Joseph Albercht <joey@joseph-albrecht.me>
2021-09-09 00:17:33 -0400

    make changes to run-test.sh for readibility. typo in package.json

--- week7/run-test.sh
+++ week7/run-test.sh
@@ -1,18 +1,16 @@
+## parser location
+parser=$(pwd)/parser.js
+
 ## setup temp test directory
 test_contents=$1
 work_dir=`mktemp -d`
 cp -r $1 $work_dir
-
-## find full path to .vm file
-vm_file=$(find $(cd $test_contents; pwd;) | grep 'vm$')
-
-## parser location
-parser=$(pwd)/parser.js
+cd $work_dir
 
 ## parse .vm file and create a .asm file
-cd $work_dir
+vm_file=$(find $(pwd) | grep 'vm$')
 node $parser $vm_file
 
-## test .asm file
+## test generated .asm file
 test_file=$(find $(pwd) | grep -v 'VME' | grep '.tst')
 ~/home/dev/nand2tetris-materials/tools/CPUEmulator.sh $test_file
