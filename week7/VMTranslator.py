29d36509d11001bb3b5e35cd63fb97a331064861
joey <joey@joeys-MacBook-Pro.local>
2022-07-11 13:46:59 -0700

    a bit of refactoring

--- week7/VMTranslator.py
+++ week7/VMTranslator.py
@@ -11,15 +11,18 @@ class Parser():
         self.path        = os.path.abspath(relative_path)
         self.name        = os.path.splitext(os.path.basename(self.path))[0] 
         self.index       = -1
-        self.commandType = 0
+        self.commandType = None
         self.arg1        = None
         self.arg2        = None
         self.parseInput()
+        self.reset()
 
     def parseInput(self):
         with open(self.path) as f:
             self.lines = [line for line in f.read().splitlines()
-                          if line and not re.match(r"//", line)]
+                          if line
+                          and not re.match(r"//", line)
+                          and not re.match(r"^[ \t]*$", line)]
 
     def advance(self):
         self.index += 1
@@ -29,7 +32,13 @@ class Parser():
         self.arg2        = line[2] if hasIndex(line, 2) else None
 
     def hasMoreCommands(self):
-        return self.index < len(self.lines) - 1
+        return hasIndex(self.lines, self.index + 1)
+
+    def reset(self):
+        self.index       = -1
+        self.commandType = None
+        self.arg1        = None
+        self.arg2        = None
 
 
 class CodeWriter():
@@ -37,8 +46,8 @@ class CodeWriter():
         self.parser = parser
 
     def write(self):
-        with open(self.parser.name + ".asm" ,'w') as file:
-            file.write("")
+        with open(f"self.parser.name.asm ", 'w') as f:
+            f.write("")
 
 code_writer = CodeWriter(Parser(sys.argv[1]))
 code_writer.write()
